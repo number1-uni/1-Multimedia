3e31b0f1e29b286e05900bc0bda0081917fb9c47
Erlantz <96289199+GitPushErlantz@users.noreply.github.com>
2022-11-17 09:14:49 +0100

    Portrait lock

--- app/src/main/java/com/number1/phonetic/Database.java
+++ app/src/main/java/com/number1/phonetic/Database.java
@@ -8,13 +8,12 @@ import android.widget.Toast;
 import com.number1.phonetic.model.Product;
 import com.number1.phonetic.model.Supplier;
 
-import java.sql.CallableStatement;
+import java.net.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Database {
